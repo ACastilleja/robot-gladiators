2c83c429f840a49e3a4da89efd5799ba0e975aa2
ArtC <castillejaarturo@gmail.com>
2021-10-26 22:36:29 -0400

    JS practice

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is and alert! JavaScript is running!");
+var playerName=window.prompt("What is your robots's name?");
+console.log(playerName);
+console.log("THis logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+
+//this creates a function named "fight"
+
+function fight () {
+    window.alert("The fight has begun!")
+}
+
+//fight();
